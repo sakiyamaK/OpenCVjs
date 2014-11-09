3f53c9162bdc2efb24a214a92f0f72adbb9fa6c5
sakiyamaK <sakiyama.k@gmail.com>
2014-11-09 23:54:55 +0900

    行列演算の追加
    cvmCopyとcvmDoubleDiagonalization

--- javascript/opencv.js
+++ javascript/opencv.js
@@ -536,6 +536,19 @@ function cvCreateIdentityMat(rows, cols){
     
 }
 
+//CvMatのコピー
+//入力
+//mat CvMat型
+//出力
+//CvMat型
+function cvmCopy(mat){
+    var rtn = cvCreateMat(mat.rows, mat.cols);
+    for(var i = 0 ; i < rtn.rows * rtn.cols ; i++){
+        rtn.vals[i] = mat.vals[i];
+    }
+    return rtn;
+}
+
 
 //行列に値を代入
 //入力
@@ -1290,6 +1303,258 @@ function cvmEigen(mat, eps, maxLoop){
     
     return rtn;
 }
+
+//行列の二重対角化
+//入力
+//mat CvMat型 対角化する行列
+//eps double型 計算精度 デフォルト CV_DEF_EPS
+function cvmDoubleDiagonalization(mat, eps){
+    var rtn = null;
+    try{
+        //バリデーション
+        if(cvUndefinedOrNull(mat))
+            throw "mat" + ERROR.IS_UNDEFINED_OR_NULL;
+        
+        //デフォルト値
+        if(cvUndefinedOrNull(eps)) eps = CV_DEF_EPS;
+        
+        //--内部で使う処理を関数化--
+        //行列から指定した範囲の行列を抜き出す
+        function partMatrix(mat, sx, sy, ex, ey){
+            
+            var mcols = ex - sx;
+            var mrows = ey - sy;
+            
+            var mar = cvCreateMat(mrows, mcols);
+            for(var i = 0 ; i < mar.rows ; i++){
+                for(var j = 0 ; j < mar.cols ; j++){
+                    mar.vals[j + i * mar.cols] = mat.vals[sx + j + (sy + i) * mat.cols];
+                }
+            }
+            
+            return mar;
+        }
+        //行列の指定された範囲内のうちの要素の最大値とその座標を返す
+        function maxAbsAndXY(ar, sx, sy, ex, ey){
+            //デフォルト値
+            if(cvUndefinedOrNull(sx)) sx = 0;
+            if(cvUndefinedOrNull(sy)) sy = 0;
+            if(cvUndefinedOrNull(ex)) ex = ar.cols;
+            if(cvUndefinedOrNull(ey)) ey = ar.rows;
+            
+            var max = Math.abs(ar.vals[sx + sy * ar.cols]);
+            var mx = sx;
+            var my = sy;
+            for(var y = sy ; y < ey ; y++){
+                for(var x = sx ; x < ex ; x++){
+                    var tmp = Math.abs(ar.vals[x + y * ar.cols]);
+                    if(max < tmp){
+                        max = tmp;
+                        mx = x;
+                        my = y;
+                    }
+                }
+            }
+            
+            return [max, mx, my];
+        }
+        
+        //行列の指定した列ベクトルのノルムの二乗
+        function norm2MatrixCol(mat, col, start, end){
+            //デフォルト値
+            if(cvUndefinedOrNull(start)) start = 0;
+            if(cvUndefinedOrNull(end)) end = mat.rows;
+            
+            var vec = cvCreateMat(end - start, 1);
+            for(var i = 0 ; i < vec.rows ; i++){
+                vec.vals[i] = mat.vals[col + (i + start) * mat.cols];
+            }
+            
+            return cvmNorm(vec, null, CV_NORM.L2Square);
+        }
+        
+        //行列の指定した行ベクトルのノルムの二乗
+        function norm2MatrixRow(mat, row, start, end){
+            
+            //デフォルト値
+            if(cvUndefinedOrNull(start)) start = 0;
+            if(cvUndefinedOrNull(end)) end = mat.rows;
+            
+            var vec = cvCreateMat(end - start, 1);
+            for(var i = 0 ; i < vec.rows ; i++){
+                vec.vals[i] = mat.vals[i + start + row * mat.cols];
+            }
+            
+            return cvmNorm(vec, null, CV_NORM.L2Square);
+        }
+        
+        //行列の列ベクトルと縦ベクトルのhouseholder変換行列
+        function householderMatColVec(mat, col, vec){
+            var rtn = null;
+            try{
+                if(mat.rows != vec.rows)
+                    throw "matとvec" + ERROR.DIFFERENT_LENGTH;
+                
+                var matV = cvCreateMat(mat.rows, 1);
+                for(var i = 0 ; i < matV.rows ; i++){
+                    matV.vals[i] = mat.vals[col + i * mat.cols];
+                }
+                
+                rtn = cvmHouseHolder(matV, vec);
+            }
+            catch(ex){
+                alert("householderMatColVec : " + ex);
+            }
+            return rtn;
+        }
+        
+        //行列の行ベクトルと横ベクトルのhouseholder変換行列(ただし１次元配列として返す)
+        function householderMatRowVec(mat, row, vec){
+            var rtn = null;
+            try{
+                if(mat.cols != vec.cols)
+                    throw "matとvec" + ERROR.DIFFERENT_LENGTH;
+                
+                var matV = cvCreateMat(1, mat.cols);
+                var rw = row * mat.cols;
+                for(var i = 0 ; i < matV.cols ; i++){
+                    matV.vals[i] = mat.vals[i + rw];
+                }
+                
+                rtn = cvmHouseHolder(matV, vec);
+            }
+            catch(ex){
+                alert("householderMatRowVec : " + ex);
+            }
+            
+            return rtn;
+        }
+        //----------------------
+
+        //matのコピー
+        var rtn = cvmCopy(mat);
+        
+        var sx = sy = 0;
+        while(true){
+            
+            //ar^(times)段階目の対角化を行う小行列を取得
+            var mar = partMatrix(rtn, sx, sy, rtn.cols, rtn.rows);
+            var mwidth = rtn.cols - sx;
+            var mheight = rtn.rows - sy;
+            
+            var maxVXY;
+            var vec;
+            var hhMat;
+
+            //step6に到達するまでループ 基本はstep1 ~ 6と進む
+            var step = 1;
+            while(step != -1){
+                switch(step){
+                    case 1://ar^(times)の要素で絶対値が最大とるなる要素を探す。その値がeps以下なら二重対角化終了
+                        
+                        maxVXY = maxAbsAndXY(mar, mwidth);
+                        if(maxVXY[0] < eps){
+                            dWrite(0, "finish");
+                            step = -1; //二重対角化終了
+                            break;
+                        }
+                        
+                        step = 2;
+                        
+                        break;
+                        
+                    case 2://絶対値最大の要素を含む行とar^(times)の第１行を入れ替える
+                        for(var i = 0 ; i < mar.cols ; i++){
+                            var tmp = mar.vals[i];
+                            mar.vals[i] = mar.vals[i + maxVXY[2] * mar.cols];
+                            mar.vals[i + maxVXY[2] * mar.cols] = tmp;
+                        }
+                        
+                        step = 3;
+                        
+                        break;
+                        
+                    case 3://ar^(times)の第１列において、絶対値が最大となる要素を探す。その値がeps以下ならd_times=0とおきstep6へ
+                        maxVXY = maxAbsAndXY(mar, 0, 0, 1, mheight);
+                        
+                        if(maxVXY[0] > eps){
+                            step = 4;
+                        }
+                        else{
+                            mar[0] = 0;
+                            step = 6;
+                        }
+                        
+                        break;
+                        
+                    case 4://ar^(times)の第１列の第２項以下を0とするようなハウスホルダー変換を左からar^(times)に行う
+                        vec = cvCreateMat(mheight, 1);
+                        vec.vals[0] = Math.sqrt(norm2MatrixCol(mar, 0));
+                        for(var i = 1 ; i < vec.rows ; vec.vals[i++] = 0);
+                        
+                        hhMat = householderMatColVec(mar, 0, vec);
+                        
+                        mar = cvmMul(hhMat, mar);
+                        
+                        //最後の２行２列の場合は列方向だけハウスホルダー変換し、対角化が終了となる
+                        step = sx == mat.cols - 2 ? -1 : 5;
+                        
+                        break;
+                        
+                    case 5://ar^(times)の第１行において第２項以降で絶対値が最大となる要素を探す。その値がeps以下なら第１行第２項を0とおきstep7へ
+                        
+                        maxVXY = maxAbsAndXY(mar, 1, 0, mwidth, 1);
+                        
+                        if(maxVXY[0] > eps) step = 6;
+                        else{
+                            mar.vals[1] = 0;
+                            step = -1;
+                        }
+                        
+                        break;
+                        
+                    case 6://ar^(times)の第１行の第３項以降を０とするようなハウスホルダー変換を右から行う
+                        //[todo]
+                        vec = cvCreateMat(1, mwidth);
+                        vec.vals[0] = mar.vals[0];
+                        vec.vals[1] = Math.sqrt(norm2MatrixRow(mar, 0, 1, mwidth));
+                        for(var i = 2 ; i < vec.cols ; vec.vals[i++] = 0);
+                        
+                        hhMat = householderMatRowVec(mar, 0, vec);
+                        
+                        mar = cvmMul(mar, hhMat);
+
+                        step = -1;
+                        
+                        break;
+                    default://ループ用パラメータ更新
+                        throw "ありえないstepが実行されました";
+                        break;
+                }
+            }
+            
+            //コピー
+            for(var i = 0 ; i < mheight ; i++){
+                for(var j = 0 ; j < mwidth ; j++){
+                    rtn.vals[sx + j + (sy + i) * rtn.cols] = mar.vals[j + i * mwidth];
+                }
+            }
+            
+            sx++;
+            sy++;
+            if(sx == mat.cols - 1 || sy == mat.rows - 1){ //終了条件
+                break;
+            }
+        }
+    }
+    catch(ex){
+        alert("cvmDoubleDiagonalization : " + ex);
+    }
+
+    return rtn;
+}
+
+
 
 //チャンネルを合成する
 //入力
