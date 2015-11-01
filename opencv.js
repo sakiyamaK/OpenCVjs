b24b8d8cf093d4299992f47d2daa20ac01378591
崎山 圭 <sakiyama.k@gmail.com>
2015-11-01 19:45:38 +0900

    パスの指定方法の変更

--- opencv.js
+++ opencv.js
@@ -1,16 +1,16 @@
 
 //外部jsの読み込み
-document.write('<script type="text/javascript" charset="UTF-8" src="' + OPENCVJS_MODULE_PATH + 'core.js"></script>');
-document.write('<script type="text/javascript" charset="UTF-8" src="' + OPENCVJS_MODULE_PATH + 'imgcodecs.js"></script>');
-document.write('<script type="text/javascript" charset="UTF-8" src="' + OPENCVJS_MODULE_PATH + 'imgproc.js"></script>');
-document.write('<script type="text/javascript" charset="UTF-8" src="' + OPENCVJS_MODULE_PATH + 'ml.js"></script>');
-document.write('<script type="text/javascript" charset="UTF-8" src="' + OPENCVJS_MODULE_PATH + 'highgui.js"></script>');
+document.write('<script type="text/javascript" charset="UTF-8" src="' + OPENCVJS_PATH + 'module/core.js"></script>');
+document.write('<script type="text/javascript" charset="UTF-8" src="' + OPENCVJS_PATH + 'module/imgcodecs.js"></script>');
+document.write('<script type="text/javascript" charset="UTF-8" src="' + OPENCVJS_PATH + 'module/imgproc.js"></script>');
+document.write('<script type="text/javascript" charset="UTF-8" src="' + OPENCVJS_PATH + 'module/ml.js"></script>');
+document.write('<script type="text/javascript" charset="UTF-8" src="' + OPENCVJS_PATH + 'module/highgui.js"></script>');
 
 //開発中メソッド
-document.write('<script type="text/javascript" charset="UTF-8" src="' + OPENCVJS_MODULE_PATH + '00_develop.js"></script>');
+document.write('<script type="text/javascript" charset="UTF-8" src="' + OPENCVJS_PATH + 'module/00_develop.js"></script>');
 
 //test用メソッド
-document.write('<script type="text/javascript" charset="UTF-8" src="' + OPENCVJS_MODULE_PATH + 'test.js"></script>');
+document.write('<script type="text/javascript" charset="UTF-8" src="' + OPENCVJS_PATH + 'module/test.js"></script>');
 
 //メモ
 //imgタグの画像から直接IplImage型へは変換できない
